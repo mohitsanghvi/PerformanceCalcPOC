da8a3d419320f4c6e6e04c53ced8e2ec40fdcf4c
Mohit Sanghvi <mohitsanghvi96@gmail.com>
2022-06-27 11:27:43 +0530

    Added Spring injection to cucumber test framework. WIP: Integrating Cucumber runner with Spring AOP Runner.

--- src/test/java/com/example/PerformanceCalcPOC/RunnerTest.java
+++ src/test/java/com/example/PerformanceCalcPOC/RunnerTest.java
@@ -1,17 +1,12 @@
 package com.example.PerformanceCalcPOC;
 
-import com.example.PerformanceCalcPOC.steps.SearchbarSteps;
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
         glue = "com/example/PerformanceCalcPOC/steps")
-public class RunnerTest {
-
+public class RunnerTest{
 }
